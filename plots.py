baa804e53b3c57632b43cfe5fc6fd978cb7138b8
erickurquilla1999 <erickurquilla1999@gmail.com>
2022-03-30 09:53:32 -0600

    create a header file for density matrix class

--- plots.py
+++ plots.py
@@ -4,13 +4,14 @@ rho_electron=[]
 time=[]
 
 for i in range(0,100):
-    data = np.loadtxt("step_"+str(i)+".dat", unpack = True)
+    data = np.loadtxt("/home/centroescolarjuanabarrera/tesis/v_o_s/output/step_"+str(i)+".dat", unpack = True)
     rho_electron.append(data[0][0])
     time.append(data[2][0])
 
 import matplotlib.pyplot as plt
 plt.plot(time,rho_electron)
 plt.show()
+
 
 M_PI=np.pi
 #m1=0.049*1.782662e-36
